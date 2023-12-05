71d93951b32e822ec905f3d8c242d3ec6b6b1d59
Wittawas W <start@wittawasw.com>
2023-12-05 12:11:49 +0700

    add turbo_stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -31,6 +31,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        format.turbo_stream
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
