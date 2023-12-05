e200f08e1ddc65c4235132454caa3b9bdabedf6b
Wittawas W <start@wittawasw.com>
2023-12-05 10:26:30 +0700

    add fonts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+  def tag_pills(tags_string)
+    tags_string.split(',')
+               .map { |tag| "<div class='pill'>#{tag}</div>" }
+               .join('')
+               .html_safe
+  end
 end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,23 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-import {GoogleCharts} from 'google-charts';
 import {alertName} from './utilities/alert';
-
-//Load the charts library with a callback
-GoogleCharts.load(drawChart);
-
-function drawChart() {
-
-    // Standard google charts functionality is available as GoogleCharts.api after load
-    const data = GoogleCharts.api.visualization.arrayToDataTable([
-        ['Chart thing', 'Chart amount'],
-        ['Lorem ipsum', 60],
-        ['Dolor sit', 22],
-        ['Sit amet', 18]
-    ]);
-    const pie_1_chart = new GoogleCharts.api.visualization.PieChart(document.getElementById('chart1'));
-    pie_1_chart.draw(data);
-}
 
 window.alertName = alertName;
