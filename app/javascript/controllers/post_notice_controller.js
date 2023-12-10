2da62b6a6ddbc837dfca674b71e41ac7078cffbd
Wittawas W <start@wittawasw.com>
2023-12-10 11:45:54 +0700

    import map trial

--- app/javascript/controllers/post_notice_controller.js
+++ app/javascript/controllers/post_notice_controller.js
@@ -1,8 +1,14 @@
 import { Controller } from "@hotwired/stimulus"
+import Toastify from 'toastify-js'
 
 export default class extends Controller {
   connect() {
     console.log("connected");
+
+    Toastify({
+      text: "This is a toast",
+      duration: 3000
+      }).showToast();
   }
 
   close() {
