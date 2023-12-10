95a7570cfcbd0eb189891e963030abe8d71410cf
Wittawas W <start@wittawasw.com>
2023-12-10 10:39:22 +0700

    turbo_frame new_post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -38,6 +38,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        format.turbo_stream
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
