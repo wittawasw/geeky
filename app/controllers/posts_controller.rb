5e510ad20ddf35513de16c33f3d933af9812da3d
Wittawas W <start@wittawasw.com>
2023-12-10 11:21:54 +0700

    editpost tb stream

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,6 +52,7 @@ class PostsController < ApplicationController
   def update
     respond_to do |format|
       if @post.update(post_params)
+        format.turbo_stream
         format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
         format.json { render :show, status: :ok, location: @post }
       else
@@ -66,6 +67,7 @@ class PostsController < ApplicationController
     @post.destroy!
 
     respond_to do |format|
+      format.turbo_stream
       format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
       format.json { head :no_content }
     end
