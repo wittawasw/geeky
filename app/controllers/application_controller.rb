91a7439d70e4d5d8e672d33fb16319383cf80ecc
Wittawas W <start@wittawasw.com>
2023-12-14 13:50:28 +0700

    fix post path with locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,15 @@ class ApplicationController < ActionController::Base
   end
 
   def set_locale
-    I18n.locale = params[:locale] || :th
-    # I18n.locale = session[:locale] || :th
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 end
